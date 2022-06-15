ef940bc95708fd7de0ccad74da9c7b820fc33af5
sansritisharma <87527606+sansritisharma@users.noreply.github.com>
2022-06-15 23:46:47 +0530

    progress

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import Confetti from 'react-confetti';
+import Button from '@mui/material/Button';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello!, Sana!</h1>
+     <h1 className="Bigbutt">Hello, Sana!</h1>
+     <div>
+     <Button variant="contained">Contained</Button>
+     </div>
       <Confetti />
     </div>
   );
 }
 
 export default App;
+
